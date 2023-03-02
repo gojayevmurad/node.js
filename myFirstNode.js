10e55c6556ed0a2d2c3562351a4fd2b5fbf986be
Murad Qocayev <muradqocayevi20@gmail.com>
2023-03-03 00:25:42 +0400

    request url || read the query string

--- myFirstNode.js
+++ myFirstNode.js
@@ -1,10 +1,33 @@
 var http = require("http");
-var dt = require("./date");
+// var dt = require("./date");
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("The date and time are currently: " + dt.myDateTime());
-    res.end();
-  })
-  .listen(8080);
+// http //! get current time && module
+//   .createServer(function (req, res) {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.write("The date and time are currently: " + dt.myDateTime());
+//     res.end();
+//   })
+//   .listen(8080);
+
+// http //! request url
+//   .createServer(function (req, res) {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.write(req.url);
+//     res.end();
+//   })
+//   .listen(8080);
+
+// var url = require("url");
+
+// http
+//   .createServer(function (req, res) {
+//     //!  http://localhost:8080/?year=2017&month=July,April,May ===>> 2017, July
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     var q = url.parse(req.url, true).query;
+//     var txt = q.year + " " + q.month;
+//     res.end(txt);
+//   })
+//   .listen(8080);
+
+
+
